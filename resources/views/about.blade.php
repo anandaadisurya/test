d61c1107a5d110ebd8ade1c1751439227754aab6
anandaadisurya <anandaadi335@gmail.com>
2024-11-05 02:50:45 +0700

    update files

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'About')
 
 @section('content')
-    <h2>About Us</h2>
+    <h2>Tentang Kami</h2>
     <p>Kami adalah perusahaan yang bergerak di bidang teknologi.</p>
 @endsection
